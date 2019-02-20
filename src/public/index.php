f3b4766008556483e53f789701e0931f38eb7e9d
Filipe Lopes <filiperocklopes@gmail.com>
2019-02-19 21:38:36 -0300

    Correção: o route cidades retorna o código do ibge em vez do id

--- src/public/index.php
+++ src/public/index.php
@@ -28,22 +28,35 @@ $app->get('/cidades', function (Request $request, Response $response, array $arg
     }catch(PDOException $e){
         echo $e->getMessage();
     }
-
-    // $array = array(
-    //     "nome" => "hoje",
-    //     "time" => "oi"
-    // );
-
-    // $response = $response->withJson($array);
-
-    // return $response;
 });
 
 $app->get('/cidades/{cidade_id}', function (Request $request, Response $response, array $args) {
     $cidade_id = $args['cidade_id'];
+
     $array = array(
         "id" => $cidade_id
     );
+
+    try{
+        $myPDO = new PDO("pgsql:host=localhost;dbname=meioambiente", "postgres", "meioambiente");
+        $myPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $myPDO->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+        $sql_query = "SELECT cod_ibge, nome FROM painel_ambiental.localidades ORDER BY nome ASC";
+        // $sql_query = "SHOW TABLES";
+
+        $sth = $myPDO->query("SET search_path TO painel_ambiental, public");
+        $sth = $myPDO->query($sql_query);
+        // $sth->execute();
+
+        $result = $sth->fetchAll(PDO::FETCH_ASSOC);
+        $response = $response->withJson($result);
+
+        return $response;
+
+    }catch(PDOException $e){
+        echo $e->getMessage();
+    }
 
     $response = $response->withJson($array);
 
