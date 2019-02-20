ac23fddb6ec838efc5c51ad8a4f258de65d4df30
Filipe Lopes <filiperocklopes@gmail.com>
2019-02-19 22:03:49 -0300

    Remoção de pastas e arquivos desnecessários

--- src/config/db.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$con_string = "host=localhost port=5432 dbname=meioambiente user=postgres password=inema";
-$mydb = pg_connect($con_string)or die("Nao foi possivel conectar");
-
-?>
--- src/public/index.php
+++ src/public/index.php
@@ -3,7 +3,6 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 require '../../vendor/autoload.php';
-// require '../config/db.php';
 
 $app = new \Slim\App;
 $app->get('/cidades', function (Request $request, Response $response, array $args) {
@@ -16,7 +15,6 @@ $app->get('/cidades', function (Request $request, Response $response, array $arg
         $sql_query = "SELECT idlocalidade, cod_ibge, nome FROM painel_ambiental.localidades ORDER BY nome ASC";
         // $sql_query = "SHOW TABLES";
 
-        $sth = $myPDO->query("SET search_path TO painel_ambiental, public");
         $sth = $myPDO->query($sql_query);
         // $sth->execute();
 
@@ -31,33 +29,35 @@ $app->get('/cidades', function (Request $request, Response $response, array $arg
 });
 
 $app->get('/cidades/{cidade_id}', function (Request $request, Response $response, array $args) {
-    // $cidade_id = $args['cidade_id'];
+    $cidade_id = $args['cidade_id'];
 
     // $array = array(
     //     "id" => $cidade_id
     // );
 
-    $data_hoje = ''
+    $data_hoje = '2019-02-08';
+    if($cidade_id){
+        try{
+            $myPDO = new PDO("pgsql:host=localhost;dbname=meioambiente", "postgres", "meioambiente");
+            $myPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $myPDO->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
-    try{
-        $myPDO = new PDO("pgsql:host=localhost;dbname=meioambiente", "postgres", "meioambiente");
-        $myPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $myPDO->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+            $sql_query = "SELECT idlocalidade, temperaturamin, temperaturamax, ventodirecao, ventomax, dataprevisao FROM painel_ambiental.previsaoclimatologica WHERE deletado = false ORDER BY dataprevisao ASC";
 
-        $sql_query = "SELECT cod_ibge, nome FROM painel_ambiental.localidades ORDER BY nome ASC";
-        // $sql_query = "SHOW TABLES";
+            $sth = $myPDO->query($sql_query);
 
-        $sth = $myPDO->query("SET search_path TO painel_ambiental, public");
-        $sth = $myPDO->query($sql_query);
-        // $sth->execute();
+            $result = $sth->fetchAll(PDO::FETCH_ASSOC);
+            $response = $response->withJson($result);
 
-        $result = $sth->fetchAll(PDO::FETCH_ASSOC);
-        $response = $response->withJson($result);
+            return $response;
 
-        return $response;
-
-    }catch(PDOException $e){
-        echo $e->getMessage();
+        }catch(PDOException $e){
+            echo $e->getMessage();
+        }
+    }else{
+        $resposta = array(
+            
+        )
     }
 
     $response = $response->withJson($array);
