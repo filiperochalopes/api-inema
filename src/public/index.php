0737534a1f834c2b5c479f2db14a0ccb8bba3b38
Filipe Lopes <filiperocklopes@gmail.com>
2019-02-19 22:53:33 -0300

    Adicionado rota cidades/cidade_id para acessar previsões por cidade

--- src/public/index.php
+++ src/public/index.php
@@ -28,23 +28,37 @@ $app->get('/cidades', function (Request $request, Response $response, array $arg
     }
 });
 
+//Rota GET para captura dos dados para painel do tmepo pelo id da cidade
 $app->get('/cidades/{cidade_id}', function (Request $request, Response $response, array $args) {
     $cidade_id = $args['cidade_id'];
 
-    // $array = array(
-    //     "id" => $cidade_id
-    // );
+    //Data de hoje para fins de teste, substituir depois pela função date() PHP
+    $data_hoje = '2019-02-10';
 
-    $data_hoje = '2019-02-08';
+    //Caso tenha algum id prossegue
     if($cidade_id){
         try{
             $myPDO = new PDO("pgsql:host=localhost;dbname=meioambiente", "postgres", "meioambiente");
             $myPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $myPDO->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
-            $sql_query = "SELECT idlocalidade, temperaturamin, temperaturamax, ventodirecao, ventomax, dataprevisao FROM painel_ambiental.previsaoclimatologica WHERE deletado = false ORDER BY dataprevisao ASC";
+            $sql_query = "SELECT idlocalidade, idtipoprevisao, temperaturamin, temperaturamax, ventodirecao, ventomax, dataprevisao FROM painel_ambiental.previsaoclimatologica WHERE deletado = false AND idlocalidade = $cidade_id AND dataprevisao >= '$data_hoje' ORDER BY dataprevisao ASC";
+
+            echo $sql_query;
 
             $sth = $myPDO->query($sql_query);
+            
+            //Verifica se existe esse id no banco de dados
+            if($sth->rowCount() > 0){
+                /* estrutura:
+                id: cidade_id,
+                nome: nome da cidade,
+                previsoes []
+                */
+
+            }else{
+                
+            }
 
             $result = $sth->fetchAll(PDO::FETCH_ASSOC);
             $response = $response->withJson($result);
@@ -55,9 +69,7 @@ $app->get('/cidades/{cidade_id}', function (Request $request, Response $response
             echo $e->getMessage();
         }
     }else{
-        $resposta = array(
-            
-        )
+
     }
 
     $response = $response->withJson($array);
